7b81099fa48f8898d721fbdb865e2180880326d7
WereDouglas <weredouglas@gmail.com>
2016-01-05 10:54:01 +0300

    cHANGED METAR
    METAR

--- application/controllers/Metar.php
+++ application/controllers/Metar.php
@@ -181,20 +181,19 @@ class Metar extends CI_Controller {
 
         $get_result = $this->Md->check($datetime, 'datetime', 'metar');
         if (!$get_result) {
-            if ($type != '') {
+             $this->session->set_flashdata('msg', '<div class="alert alert-success">                                                   
+                                                <strong>  meta has already been submitted</strong>									
+						</div>');
+            redirect('/metar', 'refresh');
+                return;
 
                // $metar = array('station' => $station, 'type' => $type, 'datetime' => $datetime, 'timezone' => 'GMT', 'wind_direction' => $wind_direction, 'wind_speed' => $wind_speed, 'unit' => $wind_unit, 'visibility' => $visibility, 'present_weather' => $present, 'cloud' => $cloud, 'air_temperature' => $air_temperature, 'humidity' => $humidity, 'dew_temperature' => $dew_temperature, 'wet_bulb' => $wet_bulb, 'station_pressure_hpa' => $station_pressure, 'sea_pressure_hpa' => $sea_pressure, 'recent_weather' => $recent_weather, 'submitted' => date('Y-m-d H:m:s'), 'user' => $this->session->userdata('username'), 'day' => $day);
                 //$this->Md->save($metar, 'metar');
                 //$log = array('user' => $this->session->userdata('username'), 'userid' => $this->session->userdata('id'), 'action' => 'save', 'details' => 'meta information saved ', 'date' => date('Y-m-d H:i:s'), 'ip' => $this->input->ip_address(), 'url' => '');
                 //$this->Md->save($log, 'logs');
-                redirect('/metar', 'refresh');
-                return;
+              
            
-                $this->session->set_flashdata('msg', '<div class="alert alert-success">                                                   
-                                                <strong>  meta has already been submitted</strong>									
-						</div>');
-                redirect('/metar', 'refresh');
-            }
+            
         }
 
         if ($station != "") {
