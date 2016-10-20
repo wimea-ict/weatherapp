6b2cbedf500f8fdcbb0f04abec914c5acf606242
mnsabagwa <nsabagwa_mary@yahoo.com>
2016-10-20 10:39:38 +0300

    Redirecting the dashboard to station status view
    station status pages

--- application/views/home.php
+++ application/views/home.php
@@ -3,7 +3,7 @@
 
     <div class="container ">
        
-            <iframe id="myframe" style="margin-top: 5px; width: 100%;" onload="resizeIframe(this)" name="myframe" frameborder="no" border="0" scrolling="no"  src="<?php echo base_url() . "index.php/welcome/start"; ?>"> </iframe>
+            <iframe id="myframe" style="margin-top: 5px; width: 100%;" onload="resizeIframe(this)" name="myframe" frameborder="no" border="0" scrolling="no"  src="<?php echo base_url() . "index.php/station/getStatus"; ?>"> </iframe> <!-- welcome/start -->
 
      
     </div>
